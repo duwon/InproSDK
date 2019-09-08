278ae0328e3e9b91e97965c5ab3cbed8f7c93842
Duwon <duwon@duwon.net>
2019-09-09 00:49:11 +0900

    시리털통신 GUI 추가

---
+++ ETC/SerialTest/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace SerialTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sp1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label28 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_PortName = new System.Windows.Forms.ComboBox();
+            this.button_COMOpen = new System.Windows.Forms.Button();
+            this.button_COMClose = new System.Windows.Forms.Button();
+            this.label_projectTitle = new System.Windows.Forms.Label();
+            this.label_ProjectVersion = new System.Windows.Forms.Label();
+            this.textBox_Debug = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_RxHex = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sp1
+            // 
+            this.sp1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sp1_DataReceived);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label28);
+            this.groupBox2.Controls.Add(this.label26);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.comboBox_PortName);
+            this.groupBox2.Controls.Add(this.button_COMOpen);
+            this.groupBox2.Controls.Add(this.button_COMClose);
+            this.groupBox2.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 54);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(256, 110);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Port 속성 설정";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label28.Location = new System.Drawing.Point(81, 76);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(72, 15);
+            this.label28.TabIndex = 48;
+            this.label28.Text = "115200 bps";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label26.Location = new System.Drawing.Point(6, 76);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(68, 15);
+            this.label26.TabIndex = 47;
+            this.label26.Text = "Baud Rate :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Serial Port :";
+            // 
+            // comboBox_PortName
+            // 
+            this.comboBox_PortName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox_PortName.DropDownWidth = 80;
+            this.comboBox_PortName.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.comboBox_PortName.FormattingEnabled = true;
+            this.comboBox_PortName.Location = new System.Drawing.Point(77, 31);
+            this.comboBox_PortName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox_PortName.Name = "comboBox_PortName";
+            this.comboBox_PortName.Size = new System.Drawing.Size(78, 23);
+            this.comboBox_PortName.Sorted = true;
+            this.comboBox_PortName.TabIndex = 1;
+            this.comboBox_PortName.MouseDown += new System.Windows.Forms.MouseEventHandler(this.comboBox_PortName_MouseDown);
+            // 
+            // button_COMOpen
+            // 
+            this.button_COMOpen.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_COMOpen.Location = new System.Drawing.Point(164, 30);
+            this.button_COMOpen.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_COMOpen.Name = "button_COMOpen";
+            this.button_COMOpen.Size = new System.Drawing.Size(80, 31);
+            this.button_COMOpen.TabIndex = 0;
+            this.button_COMOpen.Text = "OPEN";
+            this.button_COMOpen.UseVisualStyleBackColor = true;
+            this.button_COMOpen.Click += new System.EventHandler(this.button_COMOpen_Click);
+            // 
+            // button_COMClose
+            // 
+            this.button_COMClose.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_COMClose.Location = new System.Drawing.Point(164, 70);
+            this.button_COMClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_COMClose.Name = "button_COMClose";
+            this.button_COMClose.Size = new System.Drawing.Size(80, 31);
+            this.button_COMClose.TabIndex = 4;
+            this.button_COMClose.Text = "CLOSE";
+            this.button_COMClose.UseVisualStyleBackColor = true;
+            this.button_COMClose.Click += new System.EventHandler(this.button_COMClose_Click);
+            // 
+            // label_projectTitle
+            // 
+            this.label_projectTitle.AutoSize = true;
+            this.label_projectTitle.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_projectTitle.Location = new System.Drawing.Point(267, 11);
+            this.label_projectTitle.Name = "label_projectTitle";
+            this.label_projectTitle.Size = new System.Drawing.Size(216, 30);
+            this.label_projectTitle.TabIndex = 17;
+            this.label_projectTitle.Text = "시리얼 통신 프로그램";
+            // 
+            // label_ProjectVersion
+            // 
+            this.label_ProjectVersion.AutoSize = true;
+            this.label_ProjectVersion.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_ProjectVersion.Location = new System.Drawing.Point(524, 11);
+            this.label_ProjectVersion.Name = "label_ProjectVersion";
+            this.label_ProjectVersion.Size = new System.Drawing.Size(108, 30);
+            this.label_ProjectVersion.TabIndex = 18;
+            this.label_ProjectVersion.Text = "Version : ";
+            // 
+            // textBox_Debug
+            // 
+            this.textBox_Debug.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox_Debug.Location = new System.Drawing.Point(0, 0);
+            this.textBox_Debug.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_Debug.Multiline = true;
+            this.textBox_Debug.Name = "textBox_Debug";
+            this.textBox_Debug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Debug.Size = new System.Drawing.Size(408, 249);
+            this.textBox_Debug.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox_RxHex);
+            this.panel1.Controls.Add(this.textBox_Debug);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 171);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 249);
+            this.panel1.TabIndex = 20;
+            // 
+            // textBox_RxHex
+            // 
+            this.textBox_RxHex.Dock = System.Windows.Forms.DockStyle.Right;
+            this.textBox_RxHex.Location = new System.Drawing.Point(414, 0);
+            this.textBox_RxHex.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_RxHex.Multiline = true;
+            this.textBox_RxHex.Name = "textBox_RxHex";
+            this.textBox_RxHex.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_RxHex.Size = new System.Drawing.Size(386, 249);
+            this.textBox_RxHex.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 420);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label_ProjectVersion);
+            this.Controls.Add(this.label_projectTitle);
+            this.Controls.Add(this.groupBox2);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "시리얼통신";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort sp1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_PortName;
+        private System.Windows.Forms.Button button_COMOpen;
+        private System.Windows.Forms.Button button_COMClose;
+        private System.Windows.Forms.Label label_projectTitle;
+        private System.Windows.Forms.Label label_ProjectVersion;
+        private System.Windows.Forms.TextBox textBox_Debug;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_RxHex;
+    }
+}
+
---
+++ ETC/SerialTest/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialTest
+{
+    public partial class Form1 : Form
+    {
+        enum Stage : byte { START = 0, MESSAGETYPE = 1, DATA = 2, PARSING = 4, CHECKSUM = 3, SEND = 5, RESEND = 6, STOP = 7, WRITE = 8, END = 9, LENGTH = 10 };
+
+        public struct Message
+        {
+            public byte nextStage;
+            public byte type;
+            public byte destID;
+            public byte srcID;
+            public byte payloadSize;
+            public byte packetSize;
+            public byte dataCount;
+            public byte checksum;
+            public byte startFlag;
+            public byte[] data;
+
+            public Message(byte[] data)
+            {
+                this.nextStage = 0;
+                this.type = 0;
+                this.payloadSize = 0;
+                this.packetSize = 0;
+                this.checksum = 0;
+                this.data = data;
+                destID = 0;
+                srcID = 0;
+                dataCount = 5;
+                startFlag = 0;
+            }
+        }
+
+        enum Packet : byte { STX = 0xA5, ETX = 0x04, CHECKSUM = 0x00, DATA = 0x00, MAX_LENGTH = 0xf0, ERROR = 0x00, SUCCESS = 0x01 };
+        System.Collections.Concurrent.ConcurrentQueue<byte> rxBuffer = new System.Collections.Concurrent.ConcurrentQueue<byte>();
+        Message RxMessage = new Message();
+        DataPacket packet = new DataPacket();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            //패킷 데이터 저장 갯수 생성
+            RxMessage.data = new byte[(byte)Packet.MAX_LENGTH];
+
+            //컴포트 버튼 초기화
+            spButtonControl(false);
+
+            //컴포트  읽어오기
+            try
+            {
+                comboBox_PortName.Items.AddRange(SerialPort.GetPortNames());
+            }
+            catch { }
+
+            if (comboBox_PortName.Items.Count > 0)
+            {
+                comboBox_PortName.SelectedIndex = 0;
+            }
+
+            //GUI 버전정보 업데이트
+            label_ProjectVersion.Text = "GUI Version : " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Major.ToString() + "." + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Minor.ToString();
+
+        }
+
+        private void button_COMOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!sp1.IsOpen)
+                {
+                    sp1.PortName = this.comboBox_PortName.Text;
+                    sp1.BaudRate = 115200;
+
+                    sp1.RtsEnable = true;
+                    sp1.Open();
+
+                    this.spButtonControl(true);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_COMClose_Click(object sender, EventArgs e)
+        {
+            if (null != sp1)
+            {
+                if (sp1.IsOpen)
+                {
+                    sp1.Close();
+                }
+            }
+            this.spButtonControl(false);
+        }
+
+        private void comboBox_PortName_MouseDown(object sender, MouseEventArgs e)
+        {
+            comboBox_PortName.Items.Clear();
+            comboBox_PortName.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void spButtonControl(bool status)
+        {
+            button_COMOpen.Enabled = !status;
+            button_COMClose.Enabled = status;
+        }
+
+        private void sp1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                int spRxSize = sp1.BytesToRead; // 수신된 바이트 갯수
+
+                if (spRxSize != 0)              
+                {
+                    byte[] rxBuff = new byte[spRxSize];
+
+                    try
+                    {
+                        sp1.Read(rxBuff, 0, spRxSize);
+
+                        //수신 데이터 디버깅 창에 출력
+                        textBox_RxHex.AppendText(DateTime.Now.ToString("\r\n[HH:mm:ss] [RX] ") + BitConverter.ToString(rxBuff).Replace("-", " "));
+
+                        string strTemp = this.AddBytes(rxBuff.ToList()).Replace("\r", "").Replace("\n", Environment.NewLine);
+                        textBox_Debug.AppendText(strTemp);
+
+                        for (int i = 0; i < spRxSize; i++)
+                        {
+                            rxBuffer.Enqueue(rxBuff[i]);
+                        }
+                        ProcessMessage();
+                    }
+                    catch { }
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+
+        private void ProcessMessage()
+        {
+            while ((rxBuffer.Count != 0) || (RxMessage.nextStage == (byte)Stage.PARSING) || (RxMessage.nextStage == (byte)Stage.CHECKSUM))
+            {
+                switch (RxMessage.nextStage)
+                {
+                    case (byte)Stage.START:
+                        {
+
+                            byte _tch;
+                            for (int i = 0; i < rxBuffer.Count; i++)
+                            {
+                                rxBuffer.TryDequeue(out _tch);
+                                if (_tch == (byte)Packet.STX)
+                                {
+                                    RxMessage.startFlag++;
+                                }
+                                if (RxMessage.startFlag == 2)
+                                {
+                                    RxMessage.startFlag = 0;
+                                    RxMessage.nextStage = (byte)Stage.MESSAGETYPE;
+                                    RxMessage.data[0] = (byte)Packet.STX;
+                                    RxMessage.data[1] = (byte)Packet.STX;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    case (byte)Stage.MESSAGETYPE:
+                        {
+                            rxBuffer.TryDequeue(out RxMessage.data[2]);
+                            RxMessage.destID = RxMessage.data[2];
+
+                            rxBuffer.TryDequeue(out RxMessage.data[3]);
+                            RxMessage.srcID = RxMessage.data[3];
+
+                            rxBuffer.TryDequeue(out RxMessage.data[4]);
+                            RxMessage.payloadSize = RxMessage.data[4];
+                            RxMessage.packetSize = (byte)(RxMessage.payloadSize + 7);//15);// 7);
+   
+                            if (RxMessage.payloadSize != 0)                              
+                            {
+                                RxMessage.nextStage = (byte)Stage.DATA;             
+                            }
+                            else                                                    
+                            {
+                                RxMessage.nextStage = (byte)Stage.START;            /* START 단계로 돌아가기 */
+                            }
+
+                            RxMessage.nextStage = (byte)Stage.DATA;
+                            break;
+                        }
+                    case (byte)Stage.DATA:
+                        {
+                            rxBuffer.TryDequeue(out RxMessage.data[RxMessage.dataCount]);
+                            RxMessage.dataCount++;
+
+                            if (RxMessage.dataCount == RxMessage.packetSize)
+                            {
+                                RxMessage.dataCount = 5;
+                                if (RxMessage.data[RxMessage.packetSize - 1] == (byte)Packet.ETX)       /* 메시지 끝 바이트가 ETX로 끝나면 */
+                                {
+                                    RxMessage.checksum = RxMessage.data[RxMessage.packetSize - 2];      /* 체크섬 값을 프레임의 체크섬 변수에 저장하고 */
+                                    RxMessage.nextStage = (byte)Stage.CHECKSUM;                     /* CHECKSUM 단계로 넘어가기 */
+                                }
+                                else                                                                /* 메시지 끝 바이트가 ETX가 아니면 */
+                                {
+                                    RxMessage.nextStage = (byte)Stage.START;                        /* START 단계로 돌아가기 */
+                                }
+                            }
+                            break;
+
+                        }
+                    case (byte)Stage.CHECKSUM:
+                        {
+                            byte checksum = 0;                                       /* 체크섬 계산을 위한 지역 변수 선언 */
+
+                            for (int i = 0; i < RxMessage.packetSize - 2; i++)           /* 1번째 바이트부터 체크섬 앞 바이트까지 XOR */
+                            {
+                                checksum ^= RxMessage.data[i];
+                            }
+                            
+
+                            if (RxMessage.checksum == checksum)                     /* 체크섬 값이 정상이면 */
+                            {
+                                RxMessage.nextStage = (byte)Stage.PARSING;          /* Parsing 단계로 넘어가기 */
+                            }
+                            else                                                    /* 체크섬 값이 정상이 아니면 */
+                            {
+                                RxMessage.nextStage = (byte)Stage.START;            /* START 단계로 돌아가기 */
+                            }
+                            break;
+                        }
+                    case (byte)Stage.PARSING:
+                        {
+                            ParsingMessage();
+                            RxMessage.nextStage = (byte)Stage.START;
+                            break;
+                        }
+                    case (byte)Stage.END:
+                        {
+                            break;
+                        }
+                    default:
+                        break;
+
+                }
+            }
+        }
+
+        //메시지가 정상 일 경우 데이터 처리
+        private void ParsingMessage()
+        {
+            //packet.Deserialize(ref RxMessage.data);
+            //packet.stx
+            //string strTemp = this.AddBytes(rxBuff.ToList()).Replace("\r", "").Replace("\n", Environment.NewLine);
+            //textBox_Debug.AppendText(packet.stx.ToString() + packet.dest.ToString() + packet.src.ToString() + packet.payloadSize.ToString() + packet.checksum.ToString() + Encoding.Default.GetString(packet.payload) + packet.etx.ToString());
+            byte[] payloadData = new byte[64];
+            Array.Copy(RxMessage.data, 5, payloadData, 0, RxMessage.payloadSize);
+            switch (payloadData[0])
+             {
+                 case 0xC0:
+                    float temperature = BitConverter.ToSingle(payloadData, 2); ;
+                    float humidity = BitConverter.ToSingle(payloadData, 6); ;
+
+                    textBox_Debug.AppendText("\r\nSRC ID : " + RxMessage.srcID.ToString("X2")  + " Temp: "+ temperature.ToString("F") + "'C, Humi: "+ humidity.ToString("F") + "% \r\n");
+
+                    break;
+                 default:
+                     break;
+             }
+             
+        }
+
+        bool IsUTF8(byte _byte)
+        {
+            if ((_byte & 0xE0) == 0xE0) return true;
+            return false;
+
+        }
+
+        List<byte> RemainBytes = new List<byte>();
+        private String AddBytes(List<byte> _bytes)
+        {
+            RemainBytes.AddRange(_bytes);
+
+            if (this.RemainBytes.Count >= 2 && IsUTF8(this.RemainBytes[this.RemainBytes.Count - 2]))
+            {
+                String s = System.Text.Encoding.UTF8.GetString(RemainBytes.ToArray(), 0, this.RemainBytes.Count - 2);
+                RemainBytes.RemoveRange(0, this.RemainBytes.Count - 2);
+                return s;
+            }
+            else if (this.RemainBytes.Count >= 1 && IsUTF8(this.RemainBytes[this.RemainBytes.Count - 1]))
+            {
+                String s = System.Text.Encoding.UTF8.GetString(RemainBytes.ToArray(), 0, this.RemainBytes.Count - 1);
+                RemainBytes.RemoveRange(0, this.RemainBytes.Count - 1);
+                return s;
+            }
+            else
+            {
+                String s = System.Text.Encoding.UTF8.GetString(RemainBytes.ToArray(), 0, this.RemainBytes.Count);
+                RemainBytes.Clear();
+                return s;
+            }
+        }
+    }
+}
